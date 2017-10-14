54a48b80b1c354988b8060409bd41ff8b99da7c9
jwoodli1 <jwoodli1@uncc.edu>
2017-10-14 17:40:27 -0400

    Getting lever script ready
    yeah...lever script

---
+++ Engineer/Assets/Scripts/Levers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levers : MonoBehaviour
+{
+    bool on = false;
+    public float timer;
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            on = true;
+            Debug.Log("Lever on");
+            StartCoroutine("leverOff");
+        }
+    }
+
+    IEnumerator leverOff()
+    {
+        yield return new WaitForSeconds(timer);
+        on = false;
+        Debug.Log("Lever off");
+    }
+}
