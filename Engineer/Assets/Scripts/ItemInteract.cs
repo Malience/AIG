d841ee21909ba492fccb171472f61e7271cebbde
jwoodli1 <jwoodli1@uncc.edu>
2017-10-15 12:15:36 -0400

    More Lever Script
    Yeah ^

---
+++ Engineer/Assets/Scripts/ItemInteract.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace VRStandardAssets.Utils
+{
+    public class ItemInteract : MonoBehaviour
+    {
+        public event Action OnOver; //gaze on object
+        public event Action OnOut; //gave leaving object
+        public event Action OnClick;
+        public event Action OnDoubleClick;
+        public event Action OnUp; //fire1 release
+        public event Action OnDown; //fire1 press
+
+        protected bool m_IsOver;
+
+        public bool IsOver
+        {
+            get
+            {
+                return m_IsOver;
+            }
+        }
+
+        public void Over()
+        {
+            m_IsOver = true;
+            if (OnOver != null)
+                OnOver();
+        }
+
+        public void Out()
+        {
+            m_IsOver = false;
+            if (OnOut != null)
+                OnOut();
+        }
+
+        public void Click()
+        {
+            if (OnClick != null)
+                OnClick();
+        }
+
+        public void DoubleClick()
+        {
+            if (OnDoubleClick != null)
+                OnDoubleClick();
+        }
+
+        public void Up()
+        {
+            if (OnUp != null)
+                OnUp();
+        }
+
+        public void Down()
+        {
+            if (OnDown != null)
+                OnDown();
+        }
+    } //end class
+} //end namespace
--- Engineer/Assets/Scripts/Levers.cs
+++ Engineer/Assets/Scripts/Levers.cs
@@ -1,26 +1,79 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
+using VRStandardAssets.Utils;
 
-public class Levers : MonoBehaviour
+namespace VRStandardAssets.Utils
 {
-    bool on = false;
-    public float timer;
-
-    private void OnTriggerStay(Collider col)
+    public class Levers : MonoBehaviour
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        /*
+        //Code for non-VR
+        bool on = false;
+        public float timer;
+
+        private void OnTriggerStay(Collider col)
         {
-            on = true;
-            Debug.Log("Lever on");
-            StartCoroutine("leverOff");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                on = true;
+                Debug.Log("Lever on");
+                StartCoroutine("leverOff");
+            }
         }
-    }
 
-    IEnumerator leverOff()
-    {
-        yield return new WaitForSeconds(timer);
-        on = false;
-        Debug.Log("Lever off");
-    }
-}
+        IEnumerator leverOff()
+        {
+            yield return new WaitForSeconds(timer);
+            on = false;
+            Debug.Log("Lever off");
+        }
+        */
+
+        [SerializeField] private Material m_NormalMaterial;
+        [SerializeField] private Material m_OverMaterial;
+        [SerializeField] private Material m_ClickedMaterial;
+        [SerializeField] private Material m_DoubleClickMaterial;
+        [SerializeField] private ItemInteract item;
+        [SerializeField] private Renderer m_Renderer;
+
+        private void Awake()
+        {
+            m_Renderer.material = m_NormalMaterial;
+        }
+
+        private void OnEnable()
+        {
+            item.OnOver += HandleOver;
+            item.OnOut += HandleOut;
+            item.OnClick += HandleClick;
+            item.OnDoubleClick += HandleDoubleClick;
+        }
+
+        private void OnDisable()
+        {
+            item.OnOver -= HandleOver;
+            item.OnOut -= HandleOut;
+            item.OnClick -= HandleClick;
+            item.OnDoubleClick -= HandleDoubleClick;
+        }
+
+        private void HandleOver()
+        {
+            Debug.Log("Over Item");
+            m_Renderer.material = m_NormalMaterial;
+        }
+
+        private void HandleClick()
+        {
+            Debug.Log("Click Item");
+            m_Renderer.material = m_ClickedMaterial;
+        }
+
+        private void HandleDoubleClick()
+        {
+            Debug.Log("Double Click Item");
+            m_Renderer.material = m_DoubleClickMaterial;
+        }
+    } //end class
+} //end namespace
