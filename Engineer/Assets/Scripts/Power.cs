28a029067a4864002acb164192edfd2bcb98bc0b
jwoodli1 <jwoodli1@uncc.edu>
2017-10-17 19:23:50 -0400

    added power code
    yeah ^

---
+++ Engineer/Assets/Scripts/Power.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    Vector3 coalShaft = new Vector3(-0.39f, 3.61f, -4.97f );
+    public int power = 0;
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        if (collision.collider.tag == "coal")
+        {
+            collision.transform.position = coalShaft;
+            power++;
+            print("Coal in furnace");
+        }
+
+    }
+}
